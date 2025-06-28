babab0da92baa9ca31deee8382b43597750cfcc3
Gyula <erdeigyula3@gmail.com>
2025-06-28 13:00:22 +0200

    Update EnchantmentUtils.java

--- src/main/java/net/farkas/wildaside/util/EnchantmentUtils.java
+++ src/main/java/net/farkas/wildaside/util/EnchantmentUtils.java
@@ -5,27 +5,28 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 
 public class EnchantmentUtils {
     public static Holder<Enchantment> getEnchtantmentHolder(Level level, ResourceKey<Enchantment> resourceKey) {
-        return Holder.direct(getEnchantment(level, resourceKey));
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
+        return registrylookup.getOrThrow(resourceKey).getDelegate();
     }
 
     public static Holder<Enchantment> getEnchtantmentHolder(LevelAccessor level, ResourceKey<Enchantment> resourceKey) {
-        return Holder.direct(getEnchantment(level, resourceKey));
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
+        return registrylookup.getOrThrow(resourceKey).getDelegate();
     }
 
     public static Enchantment getEnchantment(Level level, ResourceKey<Enchantment> resourceKey) {
         HolderLookup.RegistryLookup<Enchantment> registrylookup = level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
-        System.out.println(registrylookup.getOrThrow(resourceKey));
         return registrylookup.getOrThrow(resourceKey).value();
     }
 
     public static Enchantment getEnchantment(LevelAccessor level, ResourceKey<Enchantment> resourceKey) {
         HolderLookup.RegistryLookup<Enchantment> registrylookup = level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
-        System.out.println(registrylookup.getOrThrow(resourceKey));
         return registrylookup.getOrThrow(resourceKey).value();
     }
 }
