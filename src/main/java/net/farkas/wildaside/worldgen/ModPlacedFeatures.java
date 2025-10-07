aaf357765b2a35d2715b9e10f3d0178dd912fef2
Gyula <erdeigyula3@gmail.com>
2025-10-07 11:31:54 +0200

    Update ModPlacedFeatures.java

--- src/main/java/net/farkas/wildaside/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/farkas/wildaside/worldgen/ModPlacedFeatures.java
@@ -2,7 +2,6 @@ package net.farkas.wildaside.worldgen;
 
 import net.farkas.wildaside.WildAside;
 import net.farkas.wildaside.block.ModBlocks;
-import net.farkas.wildaside.config.Config;
 import net.farkas.wildaside.util.HickoryColour;
 import net.farkas.wildaside.worldgen.modifier.LargePatchNoiseModifier;
 import net.minecraft.core.Direction;
